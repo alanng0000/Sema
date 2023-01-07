8fefccd199fd61aed795c6aae9e478b76c5558be
Alan <terraplatform10@gmail.com>
2023-01-07 21:49:08 +0800

    Draw

--- System.Draw/Draw.cs
+++ System.Draw/Draw.cs
@@ -5,36 +5,7 @@ namespace System.Draw;
 
 public class Draw : InfraObject
 {
-    public override bool Init()
-    {
-        base.Init();
-
-
-
-        return true;
-    }
-
-
-
-
-
-    private Rect AreaData;
-
-
-
-
-
-    public Rect Area
-    {
-        get
-        {
-            return this.AreaData;
-        }
-        set
-        {
-            this.AreaData = value;
-        }
-    }
+    public WinGraphics WinGraphics { get; set; }
 
 
 
@@ -44,6 +15,17 @@ public class Draw : InfraObject
     
     public bool Rect(Brush brush, Rect rect)
     {
+        WinRectangle u;
+
+        u = this.CreateWinRectangle(rect);
+
+
+
+        this.WinGraphics.FillRectangle(brush.WinBrush, u);
+
+
+
+
         return true;
     }
 
@@ -54,8 +36,50 @@ public class Draw : InfraObject
 
 
 
-    public bool Text(CharSpan charSpan, Font font, Color color, Pos pos)
+    public bool Text(char[] charList, InfraRange range, Font font, Color color, Pos pos)
     {
         return true;
+    }
+
+    
+
+
+
+
+    private WinRectangle CreateWinRectangle(Rect rect)
+    {
+        WinRectangle u;
+
+        u = new WinRectangle(this.CreateWinPoint(rect.Pos), this.CreateWinSize(rect.Size));
+
+
+        return u;
+    }
+
+
+
+
+
+    private WinPoint CreateWinPoint(Pos pos)
+    {
+        WinPoint u;
+        
+        u = new WinPoint(pos.Left, pos.Up);
+
+
+        return u;
+    }
+
+
+
+
+    private WinSize CreateWinSize(Size size)
+    {
+        WinSize u;
+        
+        u = new WinSize(size.Width, size.Height);
+
+
+        return u;
     }
 }

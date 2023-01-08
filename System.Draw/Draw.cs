e61287c12b151d018b20831e3a45009ffbeb16e0
Alan <terraplatform10@gmail.com>
2023-01-08 12:50:43 +0800

    Draw

--- System.Draw/Draw.cs
+++ System.Draw/Draw.cs
@@ -18,6 +18,22 @@ public class Draw : InfraObject
 
 
 
+
+    public ulong Buffer { get; set; }
+
+
+
+
+    public uint BufferStride { get; set; }
+
+
+
+
+
+    public Size Size;
+
+
+
     
 
     public bool Rect(Brush brush, Rect rect)
@@ -31,8 +47,7 @@ public class Draw : InfraObject
 
 
 
-
-            
+            this.ColorRect(colorBrush, rect);
         }
 
 
@@ -40,6 +55,30 @@ public class Draw : InfraObject
         return true;
     }
 
+
+
+
+
+    private bool ColorRect(ColorBrush brush, Rect rect)
+    {
+        uint color;
+
+
+        color = this.ColorInt(ref brush.Color);
+
+
+
+        ulong bufferPointer;
+
+        bufferPointer = this.Buffer;
+
+        uint bufferStride;
+
+        bufferStride = this.BufferStride;
+
+
+        return true;
+    }
 
 
 
@@ -54,5 +93,87 @@ public class Draw : InfraObject
 
 
         return true;
+    }
+
+
+
+
+    private uint ColorInt(ref Color color)
+    {
+        int i;
+
+        i = 0;
+
+
+
+        uint k;
+
+
+        k = 0;
+
+
+
+        k = k | this.ColorIntComp(color.Blue, i);
+
+
+        i = i + 1;
+
+
+        k = k | this.ColorIntComp(color.Green, i);
+
+
+        i = i + 1;
+
+
+        k = k | this.ColorIntComp(color.Red, i);
+
+
+        i = i + 1;
+
+
+        k = k | this.ColorIntComp(color.Alpha, i);
+
+
+
+
+
+        uint ret;
+
+        ret = k;
+
+
+        return ret;
+    }
+
+
+
+
+    private int BitPerByte { get { return 8; } }
+
+
+
+
+    private uint ColorIntComp(byte comp, int index)
+    {
+        int o;
+
+        o = this.BitPerByte;
+
+
+
+        uint k;
+
+        k = comp;
+
+        k = k << (index * o);
+
+
+
+        uint ret;
+
+        ret = k;
+
+
+        return ret;
     }
 }

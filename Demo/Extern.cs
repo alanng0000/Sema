d4e00b76e761cffe7aa24216672b89141c7f112b
Alan <terraplatform10@gmail.com>
2023-01-09 00:22:19 +0800

    Demo

--- Demo/Extern.cs
+++ Demo/Extern.cs
@@ -1,4 +1,4 @@
-namespace System.View;
+namespace Demo;
 
 
 
--- Demo/Import.cs
+++ Demo/Import.cs
@@ -1,2 +1,3 @@
 global using Object = System.Infra.Object;
 global using ExeExe = System.Exe.Exe;
+global using DllImportAttribute = System.Runtime.InteropServices.DllImportAttribute;

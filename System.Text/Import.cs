7a72bba210c42d61405392ef31340e8f9eedf45c
Alan <terraplatform10@gmail.com>
2023-01-09 16:51:51 +0800

    Text

--- System.Text/Import.cs
+++ System.Text/Import.cs
@@ -4,4 +4,3 @@ global using RangeInfra = System.Infra.RangeInfra;
 global using ListLine = System.Collections.Generic.List<System.Text.Line>;
 global using ListLineEnumerator = System.Collections.Generic.List<System.Text.Line>.Enumerator;
 global using SystemArray = System.Array;
-global using Encoding = System.Text.Encoding;

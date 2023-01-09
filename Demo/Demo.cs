231d37a5f28822effb9b9b6cdc0252d0cda783f4
Alan <terraplatform10@gmail.com>
2023-01-09 22:20:31 +0800

    Demo

--- Demo/Demo.cs
+++ Demo/Demo.cs
@@ -7,6 +7,22 @@ class Demo : Object
 {
     public int Execute()
     {
+        Frame frame;
+
+
+        frame = new Frame();
+
+
+        frame.Title = "System Demo";
+
+
+        frame.Init();
+
+
+        frame.Execute();
+
+
+
         return 0;
     }
 }
--- Demo/Import.cs
+++ Demo/Import.cs
@@ -1,2 +1,3 @@
 global using Object = System.Infra.Object;
+global using Frame = System.View.Frame;
 global using ExeExe = System.Exe.Exe;

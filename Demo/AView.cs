0330f3489a4247d54fff38ee9bb8dd8045f43bd0
Alan <terraplatform10@gmail.com>
2023-01-10 07:16:31 +0800

    Demo

---
+++ Demo/AView.cs
@@ -0,0 +1,146 @@
+namespace Demo;
+
+
+
+
+class AView : View
+{
+    public override bool Init()
+    {
+        base.Init();
+
+
+
+
+
+
+
+
+        ColorBrush blueBrush;
+
+        blueBrush = new ColorBrush();
+
+        blueBrush.Color.Alpha = Constant.This.ColorCompMax;
+
+        blueBrush.Color.Blue = Constant.This.ColorCompMax;
+
+        blueBrush.Init();
+
+
+
+
+
+
+        this.Pos.Left = 100;
+
+        this.Pos.Up = 100;
+
+        this.Size.Width = 400;
+
+        this.Size.Height = 400;
+
+
+        this.Back = blueBrush;
+
+
+
+
+
+        ColorBrush transparentBrush;
+
+        transparentBrush = new ColorBrush();
+
+        transparentBrush.Color = Constant.This.TransparentColor;
+
+        transparentBrush.Init();
+
+
+
+
+        ColorBrush greenBrush;
+
+        greenBrush = new ColorBrush();
+
+        greenBrush.Color.Alpha = Constant.This.ColorCompMax;
+
+        greenBrush.Color.Green = Constant.This.ColorCompMax;
+
+        greenBrush.Init();
+
+
+
+
+
+        Text text;
+
+        text = new Text();
+
+        text.Init();
+
+        text.Pos.Left = 100;
+
+        text.Pos.Up = 50;
+
+        text.Size.Width = 400;
+
+        text.Size.Height = 100;
+
+
+        text.Back = greenBrush;
+
+
+
+
+        string s;
+
+        s = "AAABBB HHHH KKKK OOOOO";
+
+
+
+        text.Value.Span.String = s;
+
+
+        text.Value.Span.Range.End = s.Length;
+
+
+
+        FontStyle fontStyle;
+
+        fontStyle = new FontStyle();
+
+        fontStyle.Init();
+
+        fontStyle.Underline = true;
+
+
+
+
+        Font font;
+
+        font = new Font();
+
+        font.Family = "Cascadia Mono";
+
+        font.Size = 30;
+
+        font.Style = fontStyle;
+
+
+        font.Init();
+
+
+
+        text.Font = font;
+
+
+
+
+        this.Child = text;
+
+        
+
+
+
+        return true;
+    }
+}
--- Demo/Demo.cs
+++ Demo/Demo.cs
@@ -5,7 +5,7 @@ namespace Demo;
 
 class Demo : Object
 {
-    private View View { get; set; }
+    private AView AView { get; set; }
 
 
 
@@ -30,137 +30,15 @@ class Demo : Object
 
 
 
+        AView aview;
 
-        ColorBrush blueBrush;
+        aview = new AView();
 
-        blueBrush = new ColorBrush();
+        aview.Init();
 
-        blueBrush.Color.Alpha = Constant.This.ColorCompMax;
 
-        blueBrush.Color.Blue = Constant.This.ColorCompMax;
 
-        blueBrush.Init();
-
-
-
-
-
-        View view;
-
-        view = new View();
-
-        view.Init();
-
-        view.Pos.Left = 100;
-
-        view.Pos.Up = 100;
-
-        view.Size.Width = 400;
-
-        view.Size.Height = 400;
-
-
-        view.Back = blueBrush;
-
-
-
-
-
-        ColorBrush transparentBrush;
-
-        transparentBrush = new ColorBrush();
-
-        transparentBrush.Color = Constant.This.TransparentColor;
-
-        transparentBrush.Init();
-
-
-
-
-        ColorBrush greenBrush;
-
-        greenBrush = new ColorBrush();
-
-        greenBrush.Color.Alpha = Constant.This.ColorCompMax;
-
-        greenBrush.Color.Green = Constant.This.ColorCompMax;
-
-        greenBrush.Init();
-
-
-
-
-
-        Text text;
-
-        text = new Text();
-
-        text.Init();
-
-        text.Pos.Left = 100;
-
-        text.Pos.Up = 50;
-
-        text.Size.Width = 400;
-
-        text.Size.Height = 100;
-
-
-        text.Back = greenBrush;
-
-
-
-
-        string s;
-
-        s = "AAABBB HHHH KKKK OOOOO";
-
-
-
-        text.Value.Span.String = s;
-
-
-        text.Value.Span.Range.End = s.Length;
-
-
-
-        FontStyle fontStyle;
-
-        fontStyle = new FontStyle();
-
-        fontStyle.Init();
-
-        fontStyle.Underline = true;
-
-
-
-
-        Font font;
-
-        font = new Font();
-
-        font.Family = "Cascadia Mono";
-
-        font.Size = 30;
-
-        font.Style = fontStyle;
-
-
-        font.Init();
-
-
-
-        text.Font = font;
-
-
-
-
-        view.Child = text;
-
-
-
-
-        frame.View = view;
+        frame.View = aview;
 
 
 
@@ -169,7 +47,7 @@ class Demo : Object
 
 
 
-        this.View = view;
+        this.AView = aview;
 
 
 
@@ -202,7 +80,7 @@ class Demo : Object
 
     public bool Aa()
     {
-        this.View.Size.Width = 700;
+        this.AView.Size.Width = 700;
 
 
 

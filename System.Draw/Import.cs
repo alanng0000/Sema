f2335c266391543ae84e0d31d69ef7c2a21672df
Alan <terraplatform10@gmail.com>
2023-01-09 18:22:59 +0800

    Draw

--- System.Draw/Import.cs
+++ System.Draw/Import.cs
@@ -1,6 +1,7 @@
 global using InfraObject = System.Infra.Object;
 global using InfraRange = System.Infra.Range;
 global using RangeInfra = System.Infra.RangeInfra;
+global using IntOp = System.Infra.IntOp;
 global using ReadOnlySpanChar = System.ReadOnlySpan<char>;
 global using WinGraphics = System.Drawing.Graphics;
 global using WinPoint = System.Drawing.Point;

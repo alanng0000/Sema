cdd91211ab0ca5dd6cea71d7304f2c3bae4e0303
Alan <terraplatform10@gmail.com>
2023-01-09 22:16:51 +0800

    Demo Exe

--- Demo/Exe.cs
+++ Demo/Exe.cs
@@ -5,6 +5,27 @@ namespace Demo;
 
 class Exe : ExeExe
 {
+    static int Main()
+    {
+        Exe exe;
+
+        exe = new Exe();
+
+        exe.Init();
+
+
+        int o;
+
+        o = exe.Execute();
+
+
+        return o;
+    }
+    
+
+
+
+
     protected override int ExecuteWork()
     {
         Demo demo;

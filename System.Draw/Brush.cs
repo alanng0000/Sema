57a01f5b21201f81d665b2b67620278a36e7b02f
Alan <terraplatform10@gmail.com>
2023-01-10 15:10:42 +0800

    Draw

--- System.Draw/Brush.cs
+++ System.Draw/Brush.cs
@@ -6,4 +6,18 @@ namespace System.Draw;
 public class Brush : InfraObject
 {
     internal WinBrush WinBrush { get; set; }
+
+
+
+    public bool Close()
+    {
+        this.WinBrush.Dispose();
+
+
+        this.WinBrush = null;
+
+        
+
+        return true;
+    }
 }

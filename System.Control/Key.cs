6ebf52edff505b8c4ad3d4fddc8955060c1f6c7a
Alan <terraplatform10@gmail.com>
2023-01-10 22:14:13 +0800

    Control Key

--- System.Control/Key.cs
+++ System.Control/Key.cs
@@ -26,6 +26,29 @@ public class Key : InfraObject
 
 
 
+    public byte Left { get; private set; }
+
+
+
+    public byte Up { get; private set; }
+
+
+
+    public byte Right { get; private set; }
+
+
+
+    public byte Down { get; private set; }
+
+
+
+
+    public byte Space { get; private set; }
+
+
+
+
+
     public byte Enter { get; private set; }
 
 
@@ -50,7 +73,86 @@ public class Key : InfraObject
 
 
 
+    public byte Home { get; private set; }
+
+
+
+
+
+    public byte End { get; private set; }
+
+
+
+
+
+    public byte PageUp { get; private set; }
+
+
+
+
+    public byte PageDown { get; private set; }
+
+
+
+
+
     public byte Backspace { get; private set; }
+
+
+
+
+
+    public byte LeftSquare { get; private set; }
+
+
+
+
+    public byte RightSquare { get; private set; }
+
+
+
+
+    public byte Semicolon { get; private set; }
+
+
+
+
+    public byte SingleQuote { get; private set; }
+
+
+
+
+    public byte EqualSign { get; private set; }
+
+
+
+
+    public byte Dash { get; private set; }
+
+
+
+
+    public byte Comma { get; private set; }
+
+
+
+
+    public byte Dot { get; private set; }
+
+
+
+
+    public byte Slash { get; private set; }
+
+
+
+
+    public byte BackSlash { get; private set; }
+
+
+
+
+    public byte BackTick { get; private set; }
 
 
 
@@ -67,8 +169,36 @@ public class Key : InfraObject
 
 
 
-        this.IsKeyArray = new bool[0x100];
 
+
+
+
+        this.Code = 0x25;
+
+
+
+        this.Left = this.AddCode();
+
+
+        this.Up = this.AddCode();
+
+
+        this.Right = this.AddCode();
+
+
+        this.Down = this.AddCode();
+
+
+
+        
+
+
+
+        this.Code = 0x20;
+
+
+
+        this.Space = this.AddCode();
 
 
 
@@ -119,11 +249,169 @@ public class Key : InfraObject
 
 
 
+
+        this.Code = 0x24;
+
+
+
+        this.Home = this.AddCode();
+
+
+
+
+
+        this.Code = 0x23;
+
+
+
+        this.End = this.AddCode();
+
+
+
+
+
+
+        this.Code = 0x21;
+
+
+
+        this.PageUp = this.AddCode();
+
+
+
+
+
+        this.Code = 0x22;
+
+
+
+        this.PageDown = this.AddCode();
+
+
+
+
+
+
+
+
+
         this.Code = 0x08;
 
 
 
         this.Backspace = this.AddCode();
+
+
+
+
+
+
+
+
+
+
+
+        this.Code = 0xdb;
+
+
+
+        this.LeftSquare = this.AddCode();
+
+
+
+
+
+        this.Code = 0xdd;
+    
+
+
+        this.RightSquare = this.AddCode();
+
+
+
+
+
+
+        this.Code = 0xba;
+
+
+
+        this.Semicolon = this.AddCode();
+
+
+
+
+
+
+        this.Code = 0xde;
+
+
+
+        this.SingleQuote = this.AddCode();
+        
+
+
+
+
+        this.Code = 0xbb;
+
+
+        this.EqualSign = this.AddCode();
+
+
+
+
+
+        this.Code = 0xbd;
+
+
+        this.Dash = this.AddCode();
+
+
+
+
+
+        this.Code = 0xbc;
+
+
+        this.Comma = this.AddCode();
+
+
+
+
+
+        this.Code = 0xbe;
+
+
+        this.Dot = this.AddCode();
+
+
+
+
+
+        this.Code = 0xbf;
+
+
+        this.Slash = this.AddCode();
+
+
+
+
+
+        this.Code = 0xdc;
+
+
+        this.BackSlash = this.AddCode();
+
+
+
+
+
+        this.Code = 0xc0;
+
+
+
+        this.BackTick = this.AddCode();
 
 
 
@@ -161,8 +449,6 @@ public class Key : InfraObject
 
 
 
-        this.IsKeyArray[o] = true;
-
 
 
     
@@ -177,20 +463,6 @@ public class Key : InfraObject
 
 
 
-
-
-    public bool IsKey(byte o)
-    {
-        return this.IsKeyArray[o];
-    }
-
-
-
-
-
-
-    private bool[] IsKeyArray { get; set; }
-    
 
 
 

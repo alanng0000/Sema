4ad35a2d32c2063ef5b378fc6d27ef2ee9335506
Alan <terraplatform10@gmail.com>
2023-01-17 17:41:56 +0800

    Text Char List

--- System.Text/CharList.cs
+++ System.Text/CharList.cs
@@ -3,8 +3,6 @@ namespace System.Text;
 
 
 
-
-
 public class CharList : InfraObject
 {
     private GenericList<char> List;
@@ -25,5 +23,79 @@ public class CharList : InfraObject
 
 
         return true;
+    }
+
+
+
+
+    public int Count
+    {
+        get
+        {
+            return this.List.Count;
+        }
+    }
+
+
+
+
+
+    public char Get(int index)
+    {
+        return this.List.Get(index);
+    }
+
+
+
+
+    public bool Set(int index, char item)
+    {
+        return this.List.Set(index, item);
+    }
+
+
+
+
+
+
+
+    public bool Add(char[] item, InfraRange range)
+    {
+        return this.List.Add(item, range);
+    }
+
+
+
+
+
+    public bool Insert(int index, char[] item, InfraRange range)
+    {
+        return this.List.Insert(index, item, range);
+    }
+
+
+
+
+    public bool Remove(InfraRange range)
+    {
+        return this.List.Remove(range);
+    }
+
+
+
+
+
+    public CharIter Iter()
+    {
+        CharIter iter;
+
+        iter = new CharIter();
+
+        iter.Iter = this.List.Iter();
+
+        iter.Init();
+
+
+        return iter;
     }
 }

cc78e46d5bf63e4318031d538a7e6905b4f5bc0b
Alan <terraplatform10@gmail.com>
2023-01-23 11:45:17 +0800

    Internal Draw Extern

--- System.Internal/DrawExtern.cs
+++ System.Internal/DrawExtern.cs
@@ -13,15 +13,12 @@ public static class DrawExtern
     public static extern ulong Draw_Draw_New();
 
 
-
     [DllImport(DrawDll)]
     public static extern ulong Draw_Draw_Init(ulong o);
 
 
-
     [DllImport(DrawDll)]
     public static extern ulong Draw_Draw_Final(ulong o);
-
 
 
     [DllImport(DrawDll)]
@@ -29,10 +26,8 @@ public static class DrawExtern
 
 
 
-
     [DllImport(DrawDll)]
     public static extern ulong Draw_Draw_GetHandle(ulong o);
-
 
 
     [DllImport(DrawDll)]
@@ -45,7 +40,6 @@ public static class DrawExtern
     public static extern ulong Draw_Draw_GetSize(ulong o);
 
 
-
     [DllImport(DrawDll)]
     public static extern ulong Draw_Draw_SetSize(ulong o, ulong value);
 
@@ -56,7 +50,68 @@ public static class DrawExtern
     public static extern ulong Draw_Draw_GetMethod(ulong o);
 
 
-
     [DllImport(DrawDll)]
     public static extern ulong Draw_Draw_SetMethod(ulong o, ulong value);
+
+
+
+    [DllImport(DrawDll)]
+    public static extern ulong Draw_Draw_Clear(ulong o, ulong color);
+
+
+
+    [DllImport(DrawDll)]
+    public static extern ulong Draw_Draw_Rect(ulong o, long left, long up, ulong width, ulong height, ulong brush);
+    
+
+
+
+
+    [DllImport(DrawDll)]
+    public static extern ulong Draw_Brush_New();
+
+
+    [DllImport(DrawDll)]
+    public static extern ulong Draw_Brush_Init(ulong o);
+
+
+    [DllImport(DrawDll)]
+    public static extern ulong Draw_Brush_Final(ulong o);
+
+
+    [DllImport(DrawDll)]
+    public static extern ulong Draw_Brush_Delete(ulong o);
+
+
+
+    [DllImport(DrawDll)]
+    public static extern ulong Draw_Brush_GetType(ulong o);
+
+
+    [DllImport(DrawDll)]
+    public static extern ulong Draw_Brush_SetType(ulong o, ulong value);
+
+
+
+    [DllImport(DrawDll)]
+    public static extern ulong Draw_Brush_GetValue(ulong o);
+
+
+    [DllImport(DrawDll)]
+    public static extern ulong Draw_Brush_SetValue(ulong o, ulong value);
+
+
+
+
+    [DllImport(DrawDll)]
+    public static extern ulong Draw_ColorBrush_Create();
+
+
+
+    [DllImport(DrawDll)]
+    public static extern ulong Draw_ColorBrush_GetColor(ulong o);
+
+
+    [DllImport(DrawDll)]
+    public static extern ulong Draw_ColorBrush_SetColor(ulong o, ulong value);
 }

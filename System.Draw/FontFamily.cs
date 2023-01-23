c984e4970c2f6443c2831901b6b6a3cb3fded952
Alan <terraplatform10@gmail.com>
2023-01-24 02:25:30 +0800

    Draw

--- System.Draw/FontFamily.cs
+++ System.Draw/FontFamily.cs
@@ -12,8 +12,88 @@ public class FontFamily : InfraObject
 
 
 
+
+        this.InitInternName();
+
+
+
+
+
+
         this.WinFontFamily = new WinFontFamily(this.Name);
 
+
+
+        return true;
+    }
+
+
+
+
+    private bool InitInternName()
+    {
+        InfraConvert convert;
+
+        convert = InfraConvert.This;
+
+
+
+        ulong length;
+
+        length = convert.ULong(this.Name.Length);
+
+
+
+        ulong oss;
+
+        oss = InfraExtern.New(length);
+
+
+
+
+        InternIntern ooo;
+
+        ooo = InternIntern.This;
+
+
+        ooo.CopyString(this.Name, oss);
+
+
+
+
+        ulong ss;
+
+
+        ss = InfraExtern.String_New();
+
+
+        InfraExtern.String_Init(ss);
+
+
+
+        InfraExtern.String_SetLength(ss, length);
+
+
+        InfraExtern.String_SetData(ss, oss);
+
+
+
+        this.InternName = ss;
+
+
+
+        return true;
+    }
+
+
+
+
+    private bool InitInternFamily()
+    {
+        ulong oo;
+
+        oo = DrawExtern.Draw_FontFamily_New();
+    
 
 
         return true;
@@ -28,10 +108,35 @@ public class FontFamily : InfraObject
 
 
 
+    private ulong InternName { get; set; }
 
 
-    public bool Close()
+
+
+    private ulong InternFamily { get; set; }
+
+
+
+
+
+
+    public virtual bool Final()
     {
+
+
+
+
+
+        InfraExtern.String_Final(this.InternName);
+
+
+
+        InfraExtern.String_Delete(this.InternName);
+
+
+
+
+
         this.WinFontFamily.Dispose();
 
 
@@ -46,5 +151,5 @@ public class FontFamily : InfraObject
 
 
 
-    internal WinFontFamily WinFontFamily { get; set; }
+    internal ulong Intern { get; set; }
 }

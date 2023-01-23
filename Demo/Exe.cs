0c4f2af9965cc8dc7b0999d21ee5177a3a4f8b46
Alan <terraplatform10@gmail.com>
2023-01-23 10:37:52 +0800

    Demo

--- Demo/Exe.cs
+++ Demo/Exe.cs
@@ -24,6 +24,89 @@ class Exe : ExeExe
 
 
 
+        // string s;
+
+        // s = "DEMO NOW Extern\n";
+
+
+
+        // ulong length;
+
+        // length = (ulong)s.Length;
+
+
+
+        // ulong data;
+
+        // data = InfraExtern.New(length);
+
+
+
+        // Internal oo;
+
+        // oo = Internal.This;
+
+
+        // oo.CopyString(s, data);
+
+
+
+
+
+        // ulong ss;
+        
+
+        // ss = InfraExtern.String_New();
+
+
+        // InfraExtern.String_Init(ss);
+
+
+
+        // InfraExtern.String_SetLength(ss, length);
+
+
+        // InfraExtern.String_SetData(ss, data);
+
+
+
+
+        // ulong console;
+
+        // console = InfraExtern.Console_New();
+
+
+
+        // InfraExtern.Console_Init(console);
+
+
+
+        // InfraExtern.Console_Write(console, ss);
+
+
+
+        // InfraExtern.Console_Final(console);
+
+
+
+        // InfraExtern.Console_Delete(console);
+
+
+
+
+
+        // InfraExtern.String_Final(ss);
+
+
+        // InfraExtern.String_Delete(ss);
+
+
+
+
+        // InfraExtern.Delete(data);
+
+
+
 
         int o;
 
--- Demo/Import.cs
+++ Demo/Import.cs
@@ -18,5 +18,7 @@ global using GridRow = System.View.GridRow;
 global using GridChild = System.View.GridChild;
 global using Constant = System.View.Constant;
 global using ExeExe = System.Exe.Exe;
+global using Internal = System.Internal.Internal;
+global using InfraExtern = System.Internal.InfraExtern;
 global using Stream = System.IO.Stream;
 global using FileStream = System.IO.FileStream;

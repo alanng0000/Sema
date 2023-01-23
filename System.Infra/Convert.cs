a3e43210b9fe1b65b05acdb6c129e60957b38857
Alan <terraplatform10@gmail.com>
2023-01-23 15:59:49 +0800

    Infra Convert

---
+++ System.Infra/Convert.cs
@@ -0,0 +1,61 @@
+namespace System.Infra;
+
+
+
+public class Convert : Object
+{
+    public static Convert This { get; } = CreateGlobal();
+
+
+
+
+    private static Convert CreateGlobal()
+    {
+        Convert global;
+
+
+        global = new Convert();
+
+
+        global.Init();
+
+
+
+        return global;
+    }
+
+
+
+
+
+
+
+    public ulong ULong(int a)
+    {
+        return (ulong)a;
+    }
+
+
+
+
+    public int SInt32(ulong a)
+    {
+        return (int)a;
+    }
+
+
+
+
+    public byte Byte(ulong a)
+    {
+        return (byte)a;
+    }
+
+
+
+
+    public byte CharByte(char a)
+    {
+        return (byte)a;
+    }
+}

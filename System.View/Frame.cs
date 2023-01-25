131e2ef3c696d6a7b0b756dd448633ad39a7dbff
Alan <terraplatform10@gmail.com>
2023-01-25 15:19:57 +0800

    View Frame

--- System.View/Frame.cs
+++ System.View/Frame.cs
@@ -421,7 +421,7 @@ public class Frame : CompObject
 
 
 
-        this.SetControlState(index, state);
+        this.ControlChange(index, state);
 
 
 
@@ -438,7 +438,7 @@ public class Frame : CompObject
 
 
 
-    protected virtual bool SetControlState(int index, bool state)
+    protected virtual bool ControlChange(int index, bool state)
     {
         this.Control.Set(index, state);
 

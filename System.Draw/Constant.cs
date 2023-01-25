8bd19999d19adaefc182228ccf13d9bc0249d0a2
Alan <terraplatform10@gmail.com>
2023-01-25 11:38:26 +0800

    Draw

--- System.Draw/Constant.cs
+++ System.Draw/Constant.cs
@@ -2,7 +2,7 @@ namespace System.Draw;
 
 
 
-class Constant : InfraObject
+public class Constant : InfraObject
 {
     public static Constant This { get; } = CreateGlobal();
 
@@ -30,7 +30,81 @@ class Constant : InfraObject
 
 
 
+        Infra infra;
+
+
+        infra = Infra.This;
+
+
+
+
+        this.CompMax = byte.MaxValue;
+
+
+
+
+        this.WhiteColor = infra.CreateColor(this.CompMax, this.CompMax, this.CompMax, this.CompMax);
+
+
+
+
+        this.BlackColor = infra.CreateColor(this.CompMax, 0, 0, 0);
+
+
+
+
+        this.TransparentColor = infra.CreateColor(0, 0, 0, 0);
+
+
+
 
         return true;
+    }
+
+
+
+
+
+
+
+    public Color WhiteColor
+    {
+        get;
+        private set;
+    }
+
+
+
+
+    public Color BlackColor
+    {
+        get;
+        private set;
+    }
+
+
+
+    public Color TransparentColor
+    {
+        get;
+        private set;
+    }
+
+
+
+
+
+    private byte CompMax;
+
+
+
+
+
+    public byte ColorCompMax
+    {
+        get
+        {
+            return this.CompMax;
+        }
     }
 }
--- System.Draw/Infra.cs
+++ System.Draw/Infra.cs
@@ -6,6 +6,51 @@ namespace System.Draw;
 
 public class Infra : InfraObject
 {
+    public static Infra This { get; } = CreateGlobal();
+
+
+
+
+    private static Infra CreateGlobal()
+    {
+        Infra global;
+
+        global = new Infra();
+
+        global.Init();
+
+
+        return global;
+    }
+
+
+
+
+
+    public Color CreateColor(byte alpha, byte red, byte green, byte blue)
+    {
+        Color color;
+
+        color = new Color();
+
+        color.Init();
+
+        color.Alpha = alpha;
+
+        color.Red = red;
+
+        color.Green = green;
+
+        color.Blue = blue;
+
+
+        return color;
+    }
+
+
+
+
+
     public bool BoundArea(Rect bound, ref Rect area)
     {
         int left;

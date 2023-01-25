60cf6b5774545f2336c0434c64b4b14690db97c5
Alan <terraplatform10@gmail.com>
2023-01-25 16:15:58 +0800

    Control

--- System.Control/Import.cs
+++ System.Control/Import.cs
@@ -1,5 +1,4 @@
 global using InfraObject = System.Infra.Object;
-global using InfraConvert = System.Infra.Convert;
 global using KeyCodeList = System.Intern.KeyCodeList;
 global using InternConstant = System.Intern.Constant;
 global using EventEvent = System.Event.Event;

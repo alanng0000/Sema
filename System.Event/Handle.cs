d82b9b83b036364cca506b3d0ec859fb3ed09bbb
Alan Ng <terraplatform10@gmail.com>
2023-03-02 17:01:12 +0800

    Event

--- System.Event/Handle.cs
+++ System.Event/Handle.cs
@@ -5,50 +5,6 @@ namespace System.Event;
 
 public class Handle : InfraObject
 {
-    public Int Int { get; set; }
-
-
-
-
-    public override bool Init()
-    {
-        base.Init();
-
-
-
-
-        HandleIntInfra infra;
-
-        infra = HandleIntInfra.This;
-
-
-        ulong o;
-
-
-        o = infra.NewValue();
-        
-
-
-
-
-        this.Int = new Int();
-
-
-        this.Int.Init();
-
-
-        this.Int.Value = o;
-
-
-
-
-        return true;
-    }
-
-
-
-
-
     public virtual bool Execute(object arg)
     {
         return true;
--- System.Event/Import.cs
+++ System.Event/Import.cs
@@ -1,6 +1,4 @@
 global using InfraObject = System.Infra.Object;
-global using Compare = System.Infra.Compare;
-global using Int = System.Infra.Int;
 global using IntCompare = System.Infra.IntCompare;
 global using Map = System.List.Map;
 global using MapIter = System.List.MapIter;
